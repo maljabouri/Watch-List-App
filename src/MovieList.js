16cfe4f6d980f9930a2bb242e06ddf1f7aceadb9
Ma-Add Al-Jabouri <meljaboury@outlook.com>
2023-01-26 16:47:19 +0000

    mapped each individual property of every list item to display as part of the list,(plot, title, rating) restructured files a bit, removed MovieDetails.js

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,11 +2,18 @@ import React from "react";
 
 const MovieList = (props) => {
   return (
-      <ul>
-          {props.films.map((film, index) => {
-              return <li key={index}>{film.Title}</li>
-          })}
-      </ul>
+    <ul>
+      {props.films.map((film, index) => {
+        return <div>
+          <li key={index}>{film.Title}</li>
+          <p>Rating: {film.Ratings[1].Value}</p>
+          <div class="plot">
+          <h3>Description</h3>
+          <p>{film.Plot}</p>
+          </div>
+        </div>
+      })}
+    </ul>
   );
 }
 
--- src/Watchlist.js
+++ src/Watchlist.js
@@ -1,13 +1,10 @@
 import React from "react";
 import MovieList from "./MovieList";
-import MovieDetails from "./MovieDetails";
-
 
 const WatchList = (props) => {
   return (
     <div>
       <MovieList films={props.watchList}/>
-      <MovieDetails filmDetails={props.details}/>
     </div>
   )
 }
