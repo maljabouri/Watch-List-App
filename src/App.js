ce3cce9b3dab2a3efcdef95b3f5a8c73fa5de51a
Ma-Add Al-Jabouri <meljaboury@outlook.com>
2023-01-26 15:15:37 +0000

    added plot fetch from API and stored it in watchlist state

--- src/App.js
+++ src/App.js
@@ -12,17 +12,34 @@ class App extends Component {
     this.state = {
       searchQuery: "",
       searchResults: [],
+      plot: [],
       watchList: []
     };
   }
 
   handleSearch = async (e) => {
     e.preventDefault();
-    const result = await axios.get(`http://www.omdbapi.com/?apikey=302d737f&s=${this.state.searchQuery}`)
-    this.setState({ searchResults: result.data.Search })
-    this.setState({ watchList:[...this.state.watchList, result.data.Search[0]] })
-  }
+    const searchResults = await axios.get(`http://www.omdbapi.com/?apikey=302d737f&s=${this.state.searchQuery}`);
+  
+    // Get the imdbID of the selected movie
+    const selectedMovie = searchResults.data.Search[0];
+    const imdbID = selectedMovie.imdbID;
+  
+    // Make another API call using the imdbID as a parameter
+    const movieDetails = await axios.get(`http://www.omdbapi.com/?apikey=302d737f&i=${imdbID}`);
+  
+    // Use the response data to update the watchList state
+    const updatedMovie = {
+      ...selectedMovie,
+      Plot: movieDetails.data.Plot,
+      Ratings: movieDetails.data.Ratings
+    }
+  
+    this.setState({ searchResults: searchResults.data.Search });
+    this.setState({ watchList: [...this.state.watchList, updatedMovie] });
+  };
 
+  
   handleChange = (e) => {
     this.setState({ searchQuery: e.target.value })
   }
